27fcfa0f4cc5e1c2468148e195ff2d09ce74bd8d
Aaron <asintayehu@proton.me>
2022-11-27 18:21:24 -0500

    added some timer countdown mechanisms

--- src/main.py
+++ src/main.py
@@ -10,18 +10,33 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 def start_timer():
-    countdown(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 2 == 0 and reps != 8:
+        countdown(short_break_sec)
+    elif reps == 8:
+        countdown(long_break_sec)
+    else:
+        countdown(work_sec)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
     minutes = math.floor(count / 60)
     seconds = count % 60
     if seconds == 0:
         seconds = "00"
+    elif seconds < 10:
+        seconds = f"0{seconds}"
     canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
         window.after(1000, countdown, count-1)
